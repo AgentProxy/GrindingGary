de1a4ac3599f23548d69c768cf779dcf1e94ed6e
AgentProxy <ericjoseph.flores1@gmail.com>
2017-04-26 17:16:21 +0800

    fetch

--- GG.js
+++ GG.js
@@ -1,49 +1,32 @@
-
-// var garyX=440;
-// var garyY=280;
-
-
 window.onload = function(){
-	//gary = document.getElementById('gary');
-	//console.log(gary.style.left);
 	positionMap(440, 280, "gary");
-
 	console.log(getX("gary"));
 	console.log(getY("gary"));
 		 gary();
 		 girls();
-		//positionRandom();
 }
 
 function girls(){
-	var girls = ['girl1','girl2','girl3','girl4','girl5'];
-	girlGenerator();
+	var girlList = ['girl1','girl2','girl3','girl4','girl5'];
+	girlGenerator(girlList);
+}
 
-	function girlGenerator(){
-		var min=0,max=184;
-		for (x=0; x<girls.length; x++){
-// s
-			console.log("max: " + (max-40) + " min: " + min);
-			positionMap(randX(min,(max-40)),randY(),girls[x]);
-			min=min+184;
-			max=max+184;
-// =======
-// 			positionMap(randX(min,max),randY(),girls[x]);
-// >>>>>>> 85a08e97c37c46b31129e6355f902321533bc56b
-		}
+
+function girlGenerator(girlList){
+	var min=0,max=184;
+	for (x=0; x<girlList.length; x++){
+		console.log("max: " + (max-40) + " min: " + min);
+		console.log("Overlap?: "+overlapGaryX(girls[x]) + "Girl: " + girls[x]);
+		positionMap(randX(min,(max-40)),randY(),girls[x]);
+		min=min+184;
+		max=max+184;
 	}
-
 }
 
 function randX(min,max){
 	var randX;
-// <<<<<<< HEAD
  	while(randX==null){
 		randX = (Math.random()*(max-min)) + min; 
-// =======
-	//while(randX==null||randX>880){
-	//	randX = (Math.random()*max) + min;;  
-// >>>>>>> 85a08e97c37c46b31129e6355f902321533bc56b
 		console.log("Random X: " + parseInt(randX));
 	}
 	return parseInt(randX);
@@ -51,16 +34,25 @@ function randX(min,max){
 
 function randY(){
 	var randY;
-// <<<<<<< HEAD
-// 	while(randY==null||randY>540||(randY>getY('gary')-60&&randY<getY('gary')+60)||(randY<getY('gary')-60&&randY>getY('gary')+60)){
-// =======
 	while(randY==null||randY>540){
-// >>>>>>> 85a08e97c37c46b31129e6355f902321533bc56b
 		randY = Math.random()*1000;
 		console.log("Random Y: " + parseInt(randY));
 	}
 	return parseInt(randY);
 }
+
+// function overlapGaryX(object){
+// 	object = getX(object);
+// 	gary = getX('gary');
+
+// 	if((object < (gary-40))  && (object > (gary + 80))){
+// 		return true;
+// 	}
+// 	else{
+// 		return false;
+// 	}
+
+// }
 
 function gary(){
 	 window.addEventListener('keydown',moveGary);
@@ -82,8 +74,6 @@ function moveGary(event){
 		//LEFT ARROW PRESSED
 		if(event.keyCode=="37"){
 			positionMap(-5,0,'gary');
-			//garyX=garyX-40;
-			//gary.style.left=garyX+"px";
 		}
 		//RIGHT ARROW PRESSED
 		if(event.keyCode=="39"){
